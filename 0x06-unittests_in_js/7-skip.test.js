2aef8dc3b6d683ab6390e38cf7f45f4d5f814b19
Emdinga <emdinga@gmail.com>
2024-03-01 18:45:51 +0000

    fixing 7-skip.test.js

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,11 +9,11 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
-    expect(1 === 3).to.be.true;
+  it('1 is equal to 1', () => {
+    expect(1 === 1).to.be.true;
   });
 
-  it.skip('3 is equal to 3', () => { // Skip the failing test
+  it('3 is equal to 3', () => {
     expect(3 === 3).to.be.true;
   });
 
