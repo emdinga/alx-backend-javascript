46181823115e22d4d9d326a2e0199a0f0d3391f7
Emdinga <emdinga@gmail.com>
2024-03-01 18:19:24 +0000

    fixing 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,17 +1,17 @@
 const calculateNumber = (type, a, b) => {
-	if (type === 'SUM'){
-		return Math.round(a) + Math.round(b);
-	} else if (type === 'SUBTRACT'){
-		return Math.round(a) - Math.round(b);
-	} else if (type === 'DIVIDE') {
-		const roundedB = MAth.round(b);
-		if (roundedB === 0) {
-			return 'Error';
-		}
-		return MAth.round(a) / roundedB;
-	} else {
-		throw new Error('Invalid type');
-	}
+    if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+        const roundedB = Math.round(b);
+        if (roundedB === 0) {
+            return 'Error';
+        }
+        return Math.round(a) / roundedB;
+    } else {
+        throw new Error('Invalid type');
+    }
 };
 
 module.exports = calculateNumber;
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-alx-backend-javascript
+#GitHub repository: alx-backend-javascript
 
+#Directories
 0x00. ES6 Basics
 0x01-ES6_promise
 x02-ES6_classes
 0x03-ES6_data_manipulation
 0x04-TypeScript
 0x05-Node_JS_basic
+0x06-unittests_in_js
